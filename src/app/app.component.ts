ca365c79471d7bfa2bbaa9fc9717db3a9fe37c3b
Seung Chi <seungyoonchi@gmail.com>
2017-10-21 15:55:59 -0700

    Made student object display on template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,26 @@ import { Component } from '@angular/core';
 export class Student {
   id: number;
   name: string;
+  grade: number;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1><h2>{{student.name}} details!</h2>',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{student.name}} Details!</h2>
+  <div><label>Id: </label>{{student.id}}</div>
+  <div><label>Name: </label>{{student.name}}</div>
+  <div><label>Grade: </label>{{student.grade}}</div>
+  `,
 })
 export class AppComponent {
   title = 'Directory of Students';
   student: Student = {
     id: 1,
-    name: 'Steve'
+    name: 'Steve Jobs',
+    grade: 5,
   };
 }
