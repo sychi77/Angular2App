2834914ceb8555948b2077167ff6795172371d33
Seung Chi <seungyoonchi@gmail.com>
2017-10-21 15:49:34 -0700

    Add student object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Student {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1>{{title}}</h1><h2>{{student.name}} details!</h2>',
 })
 export class AppComponent {
   title = 'Directory of Students';
-  student = 'Adam';
+  student: Student = {
+    id: 1,
+    name: 'Steve'
+  };
 }
