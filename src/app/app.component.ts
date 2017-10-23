429591f7679107adccee51caee2c43a87e64357d
Seung Chi <seungyoonchi@gmail.com>
2017-10-23 10:09:46 -0700

    getStudents function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,19 @@ import { StudentService } from './student.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [StudentService]
 })
 export class AppComponent {
   title = 'Directory of Students';
   students = Student[];
   selectedStudent: Student;
+
+  constructor(private studentService: StudentService) { }  
+
   onSelect(student: Student): void {
     this.selectedStudent = student;
+  }
+  getStudents(): void {
+    this.students = this.studentService.getStudents();
   }
 }
