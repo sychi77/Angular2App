f9d87d1cfb24c3df8d65a2b2987cdc49fd02f864
Seung Chi <seungyoonchi@gmail.com>
2017-10-25 09:50:18 -0700

    Add heroservice.gethero

--- src/app/student-detail.component.ts
+++ src/app/student-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+import { StudentService } from './student.service';
 import { Student } from './student';
 
 @Component({
@@ -19,6 +23,16 @@ import { Student } from './student';
   </div>
   `
 })
-export class StudentDetailComponent {
+export class StudentDetailComponent implements OnInit{
     @Input() student: Student;
+    constructor(
+      private studentService: StudentService,
+      private route: ActivatedRoute,
+      private location: Location
+    ) {};
+    ngOnInit(): void {
+      this.route.paramMap
+        .switchMap((params: ParamMap) => this.studentService.getStudent(+params.get('id')))
+        .subscribe(student => this.student = student);
+    }
 }
--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -7,5 +7,9 @@ import { STUDENTS } from './mock-students';
 export class StudentService {
     getStudents(): Promise<Student[]> {
         return Promise.resolve(STUDENTS);
+    };
+    getStudent(id: number): Promise<Student> {
+        return this.getStudents()
+                   .then(students => students.find(student => student.id === id));
     }
 }
