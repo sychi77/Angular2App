e525b4afb0382afc1dae12eac8f630020f814f76
Seung Chi <seungyoonchi@gmail.com>
2017-10-25 10:04:02 -0700

    Update app.module.ts with AppRoutingModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //NgModel directive
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StudentDetailComponent } from './student-detail.component';
 import { StudentsComponent } from './students.component';
 import { DashboardComponent } from './dashboard.component';
 import { StudentService } from './student.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,25 +20,7 @@ import { StudentService } from './student.service';
   imports: [
     BrowserModule,
     FormsModule, //binds [(ngModel)]
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'students',
-        component: StudentsComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: StudentDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   providers: [
     StudentService
