d611e27c6cbbd1731b96d651cab9a3630a42329e
Seung Chi <seungyoonchi@gmail.com>
2017-10-25 10:34:23 -0700

    Edit mock students names

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -1,14 +1,14 @@
 import { Student } from './student';
 
 export const STUDENTS: Student[] = [
-    { id: 11, name: 'Moe Nice', grade: 5 },
-    { id: 12, name: 'Nars Co', grade: 5 },
-    { id: 13, name: 'Bomb Asto', grade: 5 },
-    { id: 14, name: 'Cele Ritas', grade: 5 },
-    { id: 15, name: 'Mag Neta', grade: 5 },
-    { id: 16, name: 'Rubber Man', grade: 5 },
-    { id: 17, name: 'Dyn Ama', grade: 5 },
-    { id: 18, name: 'Dre IQ', grade: 5 },
-    { id: 19, name: 'Mag Ma', grade: 5 },
-    { id: 20, name: 'Tor Nado', grade: 5 }
+    { id: 11, name: 'Molly Springer', grade: 5 },
+    { id: 12, name: 'Neil Caster', grade: 5 },
+    { id: 13, name: 'Bobby Andrews', grade: 5 },
+    { id: 14, name: 'Celina Ringer', grade: 5 },
+    { id: 15, name: 'Matthew Neil', grade: 5 },
+    { id: 16, name: 'Ron Manning', grade: 5 },
+    { id: 17, name: 'David Adams', grade: 5 },
+    { id: 18, name: 'Drew Ingram', grade: 5 },
+    { id: 19, name: 'Silas Maple', grade: 5 },
+    { id: 20, name: 'Tony Nader', grade: 5 }
   ];
