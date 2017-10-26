d19d8c58dd323de42e7a771a2b5a30a93df4733e
Seung Chi <seungyoonchi@gmail.com>
2017-10-26 15:26:20 -0700

    Update getStudents function to handle http

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Student } from './student';
-import { STUDENTS } from './mock-students';
 
 @Injectable()
 export class StudentService {
-    getStudents(): Promise<Student[]> {
-        return Promise.resolve(STUDENTS);
-    };
+    private studentsUrl = 'api/students';  // URL to web api
+    
+   constructor(private http: Http) { }
+    
+   getStudents(): Promise<Student[]> {
+     return this.http.get(this.studentsUrl)
+                .toPromise()
+                .then(response => response.json().data as Student[])
+                .catch(this.handleError);
+   }
+    
+   private handleError(error: any): Promise<any> {
+     console.error('An error occurred', error); // for demo purposes only
+     return Promise.reject(error.message || error);
+   }
     getStudent(id: number): Promise<Student> {
         return this.getStudents()
                    .then(students => students.find(student => student.id === id));
